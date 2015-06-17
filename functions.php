dba8828451ad2094350eadc7c170c27642bcee57
alex chousmith <alex@ninthlink.com>
2015-06-17 11:44:56 -0700

    no header

--- functions.php
+++ functions.php
@@ -1,10 +1,16 @@
 <?php
 
 /**
- * pgb-child functions and definitions
+ * pgb-child-no-header functions and definitions
  */
 
 add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_styles' );
 function pgb_child_enqueue_styles() {
-    wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
+}
+
+/* no header */
+add_action('pgb_block_header', 'pgb_child_no_header', 1);
+function pgb_child_no_header() {
+  remove_action('pgb_block_header', 'pgb_load_block_header');
 }
